380bb011b4f876cfeef1f21f3261e6639db104b3
bruno <bruno.b.fernandes12@hotmail.com>
2024-08-15 21:56:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form')
+const valorA = document.getElementById('valorA')
+const valorB = document.getElementById('valorB')
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    
+
+})
